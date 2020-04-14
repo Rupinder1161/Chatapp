a24a23cddf32f3de295e91ff6df61f2a4c0772d5
Gurpreet Singh <gurpreet1161@gmail.com>
2020-04-14 15:59:33 +1200

    Update firebase.js

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -1,22 +1,14 @@
 import firebase from 'firebase';
 
 
-var firebaseConfig = {
-    apiKey: "AIzaSyAZSpJfkUE5e2yoiKIX9qj80qMK_KUuCtg",
-    authDomain: "dopestar-4bfa7.firebaseapp.com",
-    databaseURL: "https://dopestar-4bfa7.firebaseio.com",
-    projectId: "dopestar-4bfa7",
-    storageBucket: "dopestar-4bfa7.appspot.com",
-    messagingSenderId: "480874099249",
-    appId: "1:480874099249:web:1553ef6e730ed23ea55b10"
-  };
+
 
 
 
 const config = {
-    apiKey: "AIzaSyAZSpJfkUE5e2yoiKIX9qj80qMK_KUuCtg",
-    authDomain: "dopestar-4bfa7.firebaseapp.com",
-    databaseURL: "https://dopestar-4bfa7.firebaseio.com"
+    apiKey: "enter your key",
+    authDomain: "enter your domain",
+    databaseURL: "your database url"
   };
   firebase.initializeApp(config);
   export const auth = firebase.auth;
